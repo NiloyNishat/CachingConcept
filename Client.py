f933ee0b73ba717dd63fba3fac6e09c32522e9c9
niloy_nishat <bsse0723@iit.du.ac.bd>
2019-07-10 17:19:55 +0600

    added file to store info in slave node

--- Client.py
+++ Client.py
@@ -1,6 +1,6 @@
 import requests as req
 
-payload = {'website': 'https://abc.com'}
+payload = {'website': 'https://abcd.com'}
 resp = req.get("http://127.0.0.1:81/hello", params=payload)
 
 print(resp.text)
--- SlaveNode.py
+++ SlaveNode.py
@@ -1,17 +1,39 @@
-from flask import Flask
+import flask
 from flask import request
 from collections import defaultdict
+import csv
 
-app = Flask(__name__)
+app = flask.Flask(__name__)
 
 arr_website = defaultdict(int)
+
+
+def readFromFile():
+    with open('WebsiteLists.csv', mode='r') as file:
+        reader = csv.reader(file)
+        arr = {rows[0]: rows[1] for rows in reader}
+    file.close()
+    return arr
+
+
+def writeInFile():
+    with open('WebsiteLists.csv', 'w') as f:
+        for key in arr_website.keys():
+            f.write("%s,%s\n" % (key, arr_website[key]))
+    f.close()
 
 
 @app.route('/hello')
 def getWebsiteData():
     website = request.args.get('website')
-    arr_website[website] += 1
+    if website in arr_website:
+        val = int(arr_website[website])
+        arr_website[website] = val+1
+    else:
+        arr_website[website] = 1
+    writeInFile()
     return arr_website
 
 
+arr_website = readFromFile()
 app.run(host='0.0.0.0', port=81)
